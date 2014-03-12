59cfb247d8ec1066aa19ddae0f596d5776418dcd
Jasdev Singh <jasdev@live.in>
2014-03-12 06:56:24 -0700

    numpy practice on 12.3.14

--- numpy-practice/numpy-practice.py
+++ numpy-practice/numpy-practice.py
@@ -1,3 +1,38 @@
 import numpy as np
 
-print np.version.full_version 
+print "numpy version = ", np.version.full_version
+
+###############################################
+a = np.array([0,1,2,3,4,5])
+
+print "a = ", a
+
+print "a.ndim = ", a.ndim
+
+print "a.shape = ", a.shape
+
+###############################################
+print "Reshaping a and saving to b"
+
+b = a.reshape((3,2))
+
+print "b = ", b
+
+print "b.ndim = ", b.ndim
+
+print "b.shape = ", b.shape
+
+###############################################
+print "Reshaping a and copy() to c"
+
+c = a.reshape((3,2)).copy()
+
+print "c = ", c
+
+###############################################
+print "a*2 = ", a*2
+
+print "a**2 = ", a**2
+
+###############################################
+
