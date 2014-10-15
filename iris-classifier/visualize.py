fc4b6c8d677f16efae59761ece0e0b36645ea65d
Jasdev Singh <jasdev@live.in>
2014-10-15 13:02:48 +0530

    added visualize.py

---
+++ iris-classifier/visualize.py
@@ -0,0 +1,14 @@
+from matplotlib import pyplot as plt
+from sklearn.datasets import load_iris
+import numpy as np
+
+data = load_iris()
+features = data['data']
+feature_names = data['feature_names']
+target = data['target']
+
+for t, marker, c in zip(xrange(3), ">ox", "rgb"):
+	#plt.subplot(231)
+	plt.scatter(features[target == t, 0], features[target == t, 1], marker = marker, c = c)
+
+plt.show()
